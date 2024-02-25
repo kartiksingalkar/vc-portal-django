b0b6e6ce0f9dd4d9b88527e8c83104aa3a1c71d8
kartik <kartik@convenevc.com>
2024-02-25 12:33:55 +0530

    project setup, database changed

--- vcportal/settings.py
+++ vcportal/settings.py
@@ -75,11 +75,17 @@ WSGI_APPLICATION = 'vcportal.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'portal_django',
+        'USER': 'root',
+        'PASSWORD': 'root@123',
+        'HOST': 'localhost',
+        'PORT': '3306',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+        },
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
